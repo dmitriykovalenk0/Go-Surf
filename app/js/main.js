4056863d2d72ea469dcab74fb6e71f81c5d5dc39
dmitriykovalenk0 <dimakovalenko1999god@gmail.com>
2019-11-04 22:57:59 +0200

    Начал блок surf и сделал surf-slider

--- app/js/main.js
+++ app/js/main.js
@@ -1,19 +1,22 @@
 $(function () {
-
-
     $('.header__slider').slick({
         infinity: true,
         fade: true,
         nextArrow: '<img class="slider-arrows slider-arrows__right" src="img/arrows-right.svg"alt="">',
         prevArrow: '<img class="slider-arrows slider-arrows__left" src="img/arrows-left.svg"alt="">',
-        asNavFor: '.slider-dots'
-    });
+        asNavFor: '.slider-dots',
+    })
 
     $('.slider-dots').slick({
         slidesToShow: 4,
         slidesToScroll: 1,
-        asNavFor: '.header__slider'
+        asNavFor: '.header__slider',
     });
 
-
-});
+    $('.surf-slider').slick({
+        slidesToShow: 4,
+        slidesToScroll: 1,
+        nextArrow: '<img class="slider-arrows slider-arrows__right" src="img/arrows-right.svg"alt="">',
+        prevArrow: '<img class="slider-arrows slider-arrows__left" src="img/arrows-left.svg"alt="">',
+    });
+})
