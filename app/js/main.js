ceaa65a76051d33d8f87eb1f08100bac77221df4
dmitriykovalenk0 <dimakovalenko1999god@gmail.com>
2019-11-05 19:59:40 +0200

    Сделан блок surf-map

--- app/js/main.js
+++ app/js/main.js
@@ -18,5 +18,14 @@ $(function () {
         slidesToScroll: 1,
         nextArrow: '<img class="slider-arrows slider-arrows__right" src="img/arrows-right.svg"alt="">',
         prevArrow: '<img class="slider-arrows slider-arrows__left" src="img/arrows-left.svg"alt="">',
+        asNavFor: '.slider-map',
+    });
+
+    $('.slider-map').slick({
+        slidesToShow: 8,
+        slidesToScroll: 1,
+        arrows: false,
+        asNavFor: '.surf-slider',
+        focusOnSelect: true
     });
 })
